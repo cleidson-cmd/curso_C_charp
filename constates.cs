5fe0ef6aad8c1f62f312eee35de32ff8e2a8be52
cleidson-cmd <veioesinba@gmail.com>
2021-04-14 23:02:12 -0300

    Create constates.cs

---
+++ constates.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+class aula7{
+
+
+    static void Main(){
+        const string canal = "compartilhando conhecimento"; //constante é uma variavel que nao permite mudança de valor
+        const double pi = 3.1415;
+        Console.WriteLine("canal: {0}\nPi: {1}", canal, pi);
+
+
+    }
+}
