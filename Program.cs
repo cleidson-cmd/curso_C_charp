72bb3fe83e205f95005314260dd56eb553c28c40
cleidson-cmd <veioesinba@gmail.com>
2021-04-14 23:01:08 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace curso_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            if(args.GetLength(0)>0){
+            Console.Write(args.GetValue(0));
+            }
+        }
+
+    }
+}
