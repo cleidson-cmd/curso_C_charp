386f37ae614d357f873016b502d9391940b061ce
cleidson-cmd <veioesinba@gmail.com>
2021-04-14 23:01:17 -0300

    Create operadores.cs

---
+++ operadores.cs
@@ -0,0 +1,20 @@
+using System;
+
+class alua5{
+
+    static void Main(){
+
+        //res++   //res += 1;  //res = res + 1;
+        //bool res = 10<5;
+
+        // & = and / e
+        // | = or / ou
+
+        bool res = (5<7) | (10<5);
+
+        bool ress = (8>9) & (6<9);
+
+        Console.WriteLine(res+ " or "+" and "+ress);
+
+    }
+}
