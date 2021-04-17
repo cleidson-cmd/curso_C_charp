4e57863fce8c5dfb09f4f4548b5d29399e2a446a
cleidson-cmd <veioesinba@gmail.com>
2021-04-17 16:48:03 -0300

    Create enumerador.cs

---
+++ enumerador.cs
@@ -0,0 +1,16 @@
+using System;
+
+class aula10{
+
+    enum diasSemana{ domingo, segunda, terca, quarta, quinta, sexta, sabado};
+    static void Main(){
+
+        diasSemana ds = diasSemana.domingo;
+
+        Console.WriteLine(ds);
+
+
+
+}
+
+}
