37615ae1af876624d702181399207ee96f3cd04f
cleidson-cmd <veioesinba@gmail.com>
2021-05-04 23:18:10 -0300

    Create loop_while.cs

---
+++ loop_while.cs
@@ -0,0 +1,16 @@
+using System;
+
+class aula20{
+    static void Main(){
+
+        int[] num = new int[7];
+        int i=0;
+        while(num.Length<10){
+
+            i++;
+            Console.WriteLine("valor de i: {0} ", i);
+        }
+        
+    
+    }
+}
