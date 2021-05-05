39c7e9872ff8fd0e6ce133b6f20a6327de265430
cleidson-cmd <veioesinba@gmail.com>
2021-05-04 23:18:16 -0300

    Create loop_for.cs

---
+++ loop_for.cs
@@ -0,0 +1,13 @@
+using System;
+
+class aula19{
+    static void Main(){
+
+        int[] num = new int[5];
+        for(int i = 0; i < num.Length; i++){
+            
+            Console.WriteLine("Hello World!");    
+        }
+    
+    }
+}
