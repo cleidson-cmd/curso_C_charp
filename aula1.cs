ca7ec06e35e11b57b1a123ac63feacf53e25683b
cleidson-cmd <veioesinba@gmail.com>
2021-06-16 20:47:12 -0300

    Update aula1.cs

--- aula1.cs
+++ aula1.cs
@@ -2,7 +2,10 @@ using System;
 
 class Principal{
     static void Main(){
-        Console.WriteLine("Hello World!");
+        float A = float.Parse(Console.ReadLine());
+        float B = float.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Media = {0}", (A * 3.5) + (B * 7.5));
     
     }
 }
