02fac7ca94d415674d008e428efd3e3a4d6ca79d
cleidson-cmd <veioesinba@gmail.com>
2021-06-16 20:46:52 -0300

    Update variaveis.cs

--- variaveis.cs
+++ variaveis.cs
@@ -9,11 +9,11 @@ class Aula03{
         res = num1 + num2;
 
         byte n1 = 10; //0 e 255
-        int num=0;
-        char letra = 'c';
-        float valor = 1.7f;
+       // int num=0;
+       // char letra = 'c';
+       // float valor = 1.7f;
         string nome = "armazena textos e numeros";
-        var aux = 10; // nao espesificado o tipo da variavel, na compilaçao ela vai se basear no dado que recebe e auto definir seu tipo.
+        //var aux = 10; // nao espesificado o tipo da variavel, na compilaçao ela vai se basear no dado que recebe e auto definir seu tipo.
 
         Console.WriteLine("valor da variavel =: " + nome +" bytes "+n1 + "soma de" + num1 + " + " + num2 + " = "+ res);
     }
